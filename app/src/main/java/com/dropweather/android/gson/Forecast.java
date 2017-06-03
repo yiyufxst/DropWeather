a2786101fefe8a9ea3dbb7903226dc3deff9958a
yiyufxst <yiyufxst@alphathink.org>
2017-06-03 20:37:44 +0800

    add the function of displaying weather information

---
+++ app/src/main/java/com/dropweather/android/gson/Forecast.java
@@ -0,0 +1,33 @@
+package com.dropweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by YLX on 2017/6/3.
+ */
+
+public class Forecast {
+
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature {
+
+        public String max;
+
+        public String min;
+    }
+
+    public class More {
+
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
+
+
--- app/src/main/java/com/dropweather/android/util/Utility.java
+++ app/src/main/java/com/dropweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.dropweather.android.db.City;
 import com.dropweather.android.db.County;
 import com.dropweather.android.db.Province;
+import com.dropweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -94,5 +96,24 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * The returned JSON data is parsed into the Weather entity class.
+     *
+     * @param response
+     * @return
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
